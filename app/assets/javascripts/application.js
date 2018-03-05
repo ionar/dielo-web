fa5f99701a0a31b233c16a65bd8fe6aed7aef5b7
Ionar Rafael <irm@ionar.com.br>
2018-03-05 16:47:40 +0000

    parallax effects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,17 @@
 
 //= require_tree .
 
+
+function parallax(){
+  var layer_1 = document.getElementById('layer_1');
+  var layer_2 = document.getElementById('layer_2');
+  var layer_3 = document.getElementById('layer_3');
+  var layer_4 = document.getElementById('layer_4');
+
+  layer_1.style.top = -(window.pageYOffset / 1) + 'px';
+  layer_2.style.top = -(window.pageYOffset / 2) + 'px';
+  layer_3.style.top = -(window.pageYOffset / 1) + 'px';
+  layer_4.style.top = -(window.pageYOffset / 4) + 'px';
+}
+
+window.addEventListener("scroll", parallax, false);
