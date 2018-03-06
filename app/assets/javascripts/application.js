119341a3778604c0d3a62085d63e982b4f9cc38d
Ionar Rafael <irm@ionar.com.br>
2018-03-06 19:26:47 +0000

    add logo parallax and test with footers design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,11 +24,15 @@ function parallax(){
   var layer_2 = document.getElementById('layer_2');
   var layer_3 = document.getElementById('layer_3');
   var layer_4 = document.getElementById('layer_4');
+  var layer_5 = document.getElementById('layer_5');
+  var layer_6 = document.getElementById('layer_6');
 
   layer_1.style.top = -(window.pageYOffset / 1) + 'px';
   layer_2.style.top = -(window.pageYOffset / 2) + 'px';
   layer_3.style.top = -(window.pageYOffset / 1) + 'px';
   layer_4.style.top = -(window.pageYOffset / 4) + 'px';
+  layer_5.style.top = -(window.pageYOffset / 6) + 'px';
+  layer_6.style.top = -(window.pageYOffset / 7) + 'px';
 }
 
 window.addEventListener("scroll", parallax, false);
