2b9819fe58a361062e89efe82a9f7fa78396acae
Ionar Rafael <irm@ionar.com.br>
2018-03-12 14:33:14 +0000

    add jquery animate in cab

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,21 +18,35 @@
 
 //= require_tree .
 
+//$(document).on("page:change", function() {
+$(document).on("turbolinks:load", function() {
 
-function parallax(){
-  var layer_1 = document.getElementById('layer_1');
-  var layer_2 = document.getElementById('layer_2');
-  var layer_3 = document.getElementById('layer_3');
-  var layer_4 = document.getElementById('layer_4');
-  var layer_5 = document.getElementById('layer_5');
-  var layer_6 = document.getElementById('layer_6');
+    $("#layer_1").delay(300).animate({top: "0"}, 'slow', 'swing');
+    $("#layer_2").delay(600).animate({top: "0"}, 'slow', 'swing');
+    $("#layer_3").delay(900).animate({top: "0"}, 'slow', 'swing');
+    $("#layer_4").delay(1200).animate({top: "0"}, 'slow', 'swing');
+    $("#layer_5").delay(0).animate({top: "0"}, 'slow', 'swing');
+    $("#layer_6").delay(100).animate({top: "0"}, 'slow', 'swing');
 
-  layer_1.style.top = -(window.pageYOffset / 1) + 'px';
-  layer_2.style.top = -(window.pageYOffset / 2) + 'px';
-  layer_3.style.top = -(window.pageYOffset / 1) + 'px';
-  layer_4.style.top = -(window.pageYOffset / 4) + 'px';
-  layer_5.style.top = -(window.pageYOffset / 6) + 'px';
-  layer_6.style.top = -(window.pageYOffset / 7) + 'px';
-}
 
-window.addEventListener("scroll", parallax, false);
+// Efeito parralax cab  
+  function parallax(){
+    var layer_1 = document.getElementById('layer_1');
+    var layer_2 = document.getElementById('layer_2');
+    var layer_3 = document.getElementById('layer_3');
+    var layer_4 = document.getElementById('layer_4');
+    var layer_5 = document.getElementById('layer_5');
+    var layer_6 = document.getElementById('layer_6');
+  
+    layer_1.style.top = -(window.pageYOffset / 1) + 'px';
+    layer_2.style.top = -(window.pageYOffset / 2) + 'px';
+    layer_3.style.top = -(window.pageYOffset / 1) + 'px';
+    layer_4.style.top = -(window.pageYOffset / 4) + 'px';
+    layer_5.style.top = -(window.pageYOffset / 6) + 'px';
+    layer_6.style.top = -(window.pageYOffset / 7) + 'px';
+  }
+  window.addEventListener("scroll", parallax, false);
+// fim parallax
+
+
+});
