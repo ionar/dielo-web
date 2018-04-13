a29179295e411ba029e049271b5651b1873b52ea
Ionar Rafael <irm@ionar.com.br>
2018-04-13 22:26:42 +0000

    show products in modal

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all
+  	@products = Product.where(nil)
+  	@products = @products.pertencentes_a_linha(params[:line])
+
+  	@line = Line.find(params[:line])
+  	
+  	
   	respond_to do |format|
   		format.js
   	end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
 
+
+    scope :pertencentes_a_linha, -> (line) { where line: line }
 end
