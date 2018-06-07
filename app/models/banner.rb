5f1fc9774ccc9b11c256976429b14ddaff040968
Ionar Rafael <irm@ionar.com.br>
2018-06-07 19:12:00 +0000

    remove validates

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,7 +1,7 @@
 class Banner < ApplicationRecord
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-    validates :image, attachment_presence: true
+    #validates :image, attachment_presence: true
 
 
   default_scope { order(sequence: :asc) }
